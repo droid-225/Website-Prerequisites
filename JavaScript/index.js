84d37fa8b24c83f62cef8c2c25c9846c35859e20
Rishit Shah <aryanrishit1@gmail.com>
2025-06-24 18:30:17 +0530

    Commit 37 [Video 2: 12:31]: Learned how to log to console, add window alerts, and change content of specific elements.

---
+++ JavaScript/index.js
@@ -0,0 +1,8 @@
+console.log(`Hello`); // Can use "", '', or `` (backticks)
+console.log(`I Like Pizza!`);
+
+//window.alert(`This is an alert!`);
+//window.alert(`Did you see that!`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like pizza!!!`;
