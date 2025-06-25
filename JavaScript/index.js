048e945082d211abc63555ec59ceabf0da5969aa
Rishit Shah <aryanrishit1@gmail.com>
2025-06-25 16:26:41 +0530

    Commit 38 [Video 2: 25:19]: Learned how to use variables in JavaScript.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,8 +1,27 @@
-console.log(`Hello`); // Can use "", '', or `` (backticks)
-console.log(`I Like Pizza!`);
+//Setup
+//console.log(`Hello`); // Can use "", '', or `` (backticks)
+//console.log(`I Like Pizza!`);
 
 //window.alert(`This is an alert!`);
 //window.alert(`Did you see that!`);
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza!!!`;
+//document.getElementById("myH1").textContent = `Hello`;
+//document.getElementById("myP").textContent = `I like pizza!!!`;
+
+//Variables
+let y = 5;
+let x; //Declaration
+x = 10.5; //Assignment
+let boollin = false;
+let name = "Rishit";
+let age = 22;
+let student = true;
+
+if(boollin) {
+    console.log(`x = ${x}`);
+    console.log(`x is a ` + typeof x);
+}
+
+document.getElementById("p1").textContent = `Your Name is: ${name}`;
+document.getElementById("p2").textContent = `You Are ${age} Years Old`;
+document.getElementById("p3").textContent = `Enrolled: ` + student;
