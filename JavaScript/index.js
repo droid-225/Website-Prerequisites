a510dc71823dfed7e2acb47066261be1dac8e4fe
Rishit Shah <aryanrishit1@gmail.com>
2025-06-28 17:27:12 +0530

    Commit 47 [Video 2: 1:42:02]: Learned about the checked property in JS.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -183,3 +183,29 @@ if(age >= 18) {
 else
     console.log("You are not an adult!");
 */
+
+//Checked Property
+//.checked: Used for radio buttons and checkboxes
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const mastercardBtn = document.getElementById("mastercardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const submitBtn = document.getElementById("submitBtn");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+submitBtn.onclick = function() {
+    if(myCheckBox.checked)
+        subResult.textContent = `You are subscribed!`;
+    else
+        subResult.textContent = `You are not subscribed!`;
+
+    if(visaBtn.checked)
+        paymentResult.textContent = `You are paying with Visa.`;
+    else if(mastercardBtn.checked)
+        paymentResult.textContent = `You are paying with MasterCard.`;
+    else if(paypalBtn.checked)
+        paymentResult.textContent = `You are paying with PayPal.`;
+    else
+        paymentResult.textContent = `Please select a payment method.`
+}
