74d3e87019381b07814fef6e2d27ddfd0c6709a8
Rishit Shah <aryanrishit1@gmail.com>
2025-06-28 17:46:12 +0530

    Commit 49 [Video 2: 1:55:32]: Learned how to use switches in JS.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -214,10 +214,64 @@ submitBtn.onclick = function() {
 
 //Ternary Operator
 //Syntax: condition ? codeIfTrue : codeIfFalse;
-
+/*
 //let age = -21;
 //let message = age >= 18 ? "You are an adult!" : age < 0 ? "You can't be less than 0!" : "You are a minor!";
 let amount = 90;
 let discount = amount >= 100 ? 10 : 0;
 
 console.log(`Your total is ${amount - (amount * (discount / 100))}`);
+*/
+
+//Switch
+/*
+let day = "pizza";
+
+switch(day) {
+    case 1:
+        console.log("It's Sunday!!");
+        break;
+    case 2:
+        console.log("It's Monday!");
+        break;
+    case 3:
+        console.log("It's Tuesday!");
+        break;
+    case 4:
+        console.log("It's Wednesday!");
+        break;
+    case 5:
+        console.log("It's Thursday!");
+        break;
+    case 6:
+        console.log("It's Friday!");
+        break;
+    case 7:
+        console.log("It's Saturday!");
+        break;
+    default:
+        console.log(`${day} is not a day!`);
+}
+*/
+
+let testScore = 63;
+let letterGrade;
+
+switch(true) {
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    case testScore >= 60:
+        letterGrade = "D";
+        break;
+    default:
+        letterGrade = "F";
+}
+
+console.log(`${letterGrade}`);
