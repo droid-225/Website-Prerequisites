16580532022a04c6b2b494369a0f8a1815d2a56a
Rishit Shah <aryanrishit1@gmail.com>
2025-07-26 11:43:59 +0530

    Commit 84 [Video 2: 6:29:18]: Learned how to work with an array of objects in JS.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1233,7 +1233,7 @@ for(const property in person.address) {
     console.log(person.address[property]);
 }
 */
-
+/*
 class Person {
     constructor(name, age, ...address) {
         this.name = name;
@@ -1263,3 +1263,42 @@ const person3 = new Person("Squidward", 45, "126 Conch St.",
                                             "Int. Waters");
 
 console.log(person3.address.street);
+*/
+
+//Array of Objects
+
+const fruits = [
+    {name: "apple", color: "red", calories: 95},
+    {name: "orange", color: "orange", calories: 45},
+    {name: "banana", color: "yellow", calories: 105},
+    {name: "coconut", color: "white", calories: 159},
+    {name: "pineapple", color: "yellow", calories: 37}
+];
+
+fruits.push({name: "grapes", color: "purple", calories: 62});
+fruits.pop();
+//fruits.splice(1, 2);
+
+//console.log(fruits);
+//fruits.forEach(fruit => console.log(fruit));
+/*
+const fruitNames = fruits.map(fruit => fruit.name);
+const fruitColors = fruits.map(fruit => fruit.color);
+const fruitCalories = fruits.map(fruit => fruit.calories);
+
+console.log(fruitNames);
+console.log(fruitColors);
+console.log(fruitCalories);
+*/
+/*
+const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
+const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+console.log(yellowFruits);
+console.log(lowCalFruits);
+*/
+
+const maxFruit = fruits.reduce((max, fruit) => 
+                                fruit.calories > max.calories ? fruit : max);
+
+console.log(maxFruit);
