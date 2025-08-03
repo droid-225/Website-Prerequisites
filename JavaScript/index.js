d92bbb5253438b407285f9979efbbc7be49c691e
Rishit Shah <aryanrishit1@gmail.com>
2025-08-03 15:10:14 +0530

    Commit 105 [Video 2: 10:18:11]: Made a Rock Paper Scissors program.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2091,19 +2091,19 @@ myButton.addEventListener("click", event => {
     }
 });
 */
-
+/*
 let buttons = document.querySelectorAll(".myButtons");
 
 buttons.forEach(button => {
     button.classList.add("enabled");
 });
-
+*/
 /*
 buttons.forEach(button => {
     button.classList.remove("enabled");
 });
 */
-
+/*
 buttons.forEach(button => {
     button.addEventListener("mouseover", event => {
         event.target.classList.toggle("hover");
@@ -2126,3 +2126,56 @@ buttons.forEach(button => {
         }
     });
 });
+*/
+
+//Rock Paper Scissors Program
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const compDisplay = document.getElementById("compDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const compScoreDisplay = document.getElementById("compScoreDisplay");
+let playerScore = 0;
+let compScore = 0;
+
+function playGame(playerChoice) {
+    const compChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoice === compChoice) {
+        result = "It's a Tie!"
+    }
+    else {
+        switch(playerChoice) {
+            case "rock":
+                result = (compChoice === "scissors") ? "You Win!" : "You Lose!";
+                break;
+            case "paper":
+                result = (compChoice === "rock") ? "You Win!" : "You Lose!";
+                break;
+            case "scissors":
+                result = (compChoice === "paper") ? "You Win!" : "You Lose!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    compDisplay.textContent = `Computer: ${compChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText", "redText");
+
+    switch(result) {
+        case "You Win!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "You Lose!":
+            resultDisplay.classList.add("redText");
+            compScore++;
+            compScoreDisplay.textContent = compScore;
+            break;
+    }
+}
