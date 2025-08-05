0d241a2466d91a2242033879cba3fcba080cdcdf
Rishit Shah <aryanrishit1@gmail.com>
2025-08-05 18:01:59 +0530

    Commit 111 [Video 2: 11:21:21]: Learned how to use the fetch() function and how to fetch data from an API.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2435,9 +2435,50 @@ const parsedData = JSON.parse(jsonPeople);
 
 console.log(parsedData);
 */
-
+/*
 fetch("people.json")
     .then(response => response.json())
     .then(values => values.forEach(value => console.log(value)))
     .catch(error => console.error(error));
 // here, fetch and response.json both return promises
+*/
+
+//fetch
+// Function used for making HTTP requests to fetch resources (JSON style data, images, files).
+// Simplifies asynchronous data fetching in JS and used for interacting with APIs to retrieve
+// and send data asynchronously over the web.
+// Syntax: fetch(url, {options})
+/*
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+        if(!response.ok) {
+            throw new Error("Could not fetch resource!");
+        }
+        return response.json();
+    })
+    .then(data => console.log(data.id))
+    .catch(error => console.error(error));
+*/
+
+fetchData();
+
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    
+        if(!response.ok) {
+            throw new Error("Could not fetch resource!");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
