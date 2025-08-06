16f6af4360a2314e4f4c2284c3ea3920cebb84ce
Rishit Shah <aryanrishit1@gmail.com>
2025-08-06 18:52:09 +0530

    Commit 116: [Video 2: 52:48]: Learned about props [now deprecated in React 19+].

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
-import Button from "./Button.jsx";
+import Student from "./Student.jsx";
 
 function App() {
 
   return(
     <>
-      <Button/>
+      <Student name="Spongebob" age="hello" isStudent={true}/>
+      <Student name="Patrick" age={35} isStudent={true}/>
+      <Student name="Sandy" age={30} isStudent={false}/>
     </>
   );
 }
