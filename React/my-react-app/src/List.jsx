5a410981e83043fd6d6d124c7de6c9703175d72e
Rishit Shah <aryanrishit1@gmail.com>
2025-08-06 19:40:55 +0530

    Commit 118 [Video 3: 1:29:41]: Learned how to render lists in React.

---
+++ React/my-react-app/src/List.jsx
@@ -0,0 +1,19 @@
+
+function List(props) {
+    const itemList = props.items;
+    const category = props.category;
+
+    const listItems = itemList.map(item => <li key={item.id}>
+                                            {item.name}: &nbsp;
+                                            <b>{item.calories}</b>
+                                          </li>);
+
+    return(
+        <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+        </>
+    );
+}
+
+export default List
