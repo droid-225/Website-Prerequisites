4d8be451f293d4e4b8117f22cc1cbaa30abb16cc
Rishit Shah <aryanrishit1@gmail.com>
2025-08-06 18:13:06 +0530

    Commit 115 [Video 3: 40:37]: Learned the different ways to apply CSS styling to React components.

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,13 +1,10 @@
-import Card from './Card.jsx'
+import Button from "./Button.jsx";
 
 function App() {
 
   return(
     <>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Button/>
     </>
   );
 }
