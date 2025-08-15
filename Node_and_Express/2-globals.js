c943de148424a98b1abfa7e3780c5ec2d122d304
Rishit Shah <aryanrishit1@gmail.com>
2025-08-15 16:01:29 +0530

    Commit 146 [Video 5: 29:37]: Learned about the Globals in NodeJS.

---
+++ Node_and_Express/2-globals.js
@@ -0,0 +1,13 @@
+// Globals - No Window!
+
+// Some important Global variables:
+// __dirname : path to current directory
+// __filename : file name
+// require : function to use modules (CommonJS)
+// module : info about current module (file)
+// process : info about environment where the program is being executed
+// You can always access these variables anywhere in the program
+
+console.log(__dirname); // You can console log any of the global variables
+
+// setInterval and setTimeout also works here
