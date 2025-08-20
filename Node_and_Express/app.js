ef447ce3c88b8700a2790f2498525e4d6f4e9964
Rishit Shah <aryanrishit1@gmail.com>
2025-08-20 16:19:58 +0530

    Commit 151 [Video 5: 2:47:07]: Learned how the event loop works in NodeJS.

--- Node_and_Express/app.js
+++ Node_and_Express/app.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+console.log("First Task");
+
+setTimeout(() => {
+    console.log("Second Task");
+}, 0);
+
+for(i = 0; i < 10; i++) {
+    console.log(`Task ${i+3}`);
+}
