04c93d1dad9954056ad429d6533987c96d68962b
Rishit Shah <aryanrishit1@gmail.com>
2025-08-21 10:23:13 +0530

    Commit 152 [Video 5: 3:12:48]: Learned about async patterns, including blocking code and how to simplify code using async-await functions.

--- Node_and_Express/app.js
+++ Node_and_Express/app.js
@@ -1,9 +1,39 @@
-console.log("First Task");
+const {readFile, writeFile} = require('fs').promises; // automatically creates promises from the imported functions
+//const util = require('util'); // built-in into NodeJS
+//const readFilePromise = util.promisify(readFile);
+//const writeFilePromise = util.promisify(writeFile);
 
-setTimeout(() => {
-    console.log("Second Task");
-}, 0);
+/*
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, data) => {
+            if(err) {
+                reject(err);
+            }  
+            else {
+                resolve(data);
+            }
+        });
+    });
+};
+*/
 
-for(i = 0; i < 10; i++) {
-    console.log(`Task ${i+3}`);
-}
+/* 
+getText('./content/first.txt')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+*/
+
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-grenade.txt', 
+                        `THIS IS AWESOME : ${first} ${second}`);
+        console.log(first, second);
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+start();
