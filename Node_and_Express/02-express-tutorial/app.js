d95c1b0531dcec73f39f6380a6627b1245ad5093
Rishit Shah <aryanrishit1@gmail.com>
2025-08-27 19:55:59 +0530

    Commit 162 [Video 5: 5:50:40]: Learned how to use route parameters.

--- Node_and_Express/02-express-tutorial/app.js
+++ Node_and_Express/02-express-tutorial/app.js
@@ -3,8 +3,30 @@ const app = express();
 const {products} = require('./data');
 
 app.get('/', (req, res) => {
-    //res.json([{name: 'John'}, {name: 'Susan'}]);
-    res.json(products);
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name, image};
+    });
+
+    res.json(newProducts);
+});
+
+app.get('/api/products/:productID', (req, res) => { // the :productID is called a route parameter
+    //console.log(req);
+    //console.log(req.params); // contains parameter value
+    const {productID} = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID));
+
+    if(!singleProduct) {
+        return res.status(404).send('Product Does Not Exist!');
+    }
+
+    res.json(singleProduct);
 });
 
 app.listen(5000, () => {
